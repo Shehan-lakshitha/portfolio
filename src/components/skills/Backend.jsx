87cb640e14056ae1f74d8b5a5a7cd4f29bca115e
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-07-05 11:16:58 +0530

    Add Skills component to main App layout

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className="skills_content">
+      <h3 className="skills_title">
+        <div className="skills_box">
+          <div className="skills_group">
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name"></h3>
+                <span className="skills_level"></span>
+              </div>
+            </div>
+
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name"></h3>
+                <span className="skills_level"></span>
+              </div>
+            </div>
+
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name"></h3>
+                <span className="skills_level"></span>
+              </div>
+            </div>
+          </div>
+
+          <div className="skills_group">
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name"></h3>
+                <span className="skills_level"></span>
+              </div>
+            </div>
+
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name"></h3>
+                <span className="skills_level"></span>
+              </div>
+            </div>
+
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name"></h3>
+                <span className="skills_level"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </h3>
+    </div>
+  )
+}
+
+export default Backend
---
+++ src/components/skills/Frontend.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const Frontend = () => {
+  return (
+    <div className="skills_content">
+      <h3 className="skills_title">Frontend Developer</h3>
+
+        <div className="skills_box">
+          <div className="skills_group">
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name">ReactJs</h3>
+                <span className="skills_level">Junior</span>
+              </div>
+            </div>
+
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name">Javascript</h3>
+                <span className="skills_level">Junior</span>
+              </div>
+            </div>
+
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name">HTML</h3>
+                <span className="skills_level">Mid</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="skills_group">
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name"></h3>
+                <span className="skills_level"></span>
+              </div>
+            </div>
+
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name"></h3>
+                <span className="skills_level"></span>
+              </div>
+            </div>
+
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name"></h3>
+                <span className="skills_level"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+    </div>
+  );
+};
+
+export default Frontend;
