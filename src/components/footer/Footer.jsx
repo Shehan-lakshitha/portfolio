3b4e950de7ac9dd47cec6e35e2a231ca1f6b31e4
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-07-15 20:53:37 +0530

    feat: Add Footer component to main App layout

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer_container container">
+        <h1 className="footer_title">Shehan Lakshitha</h1>
+
+        <ul className="footer_list">
+          <li>
+            <a href="#about" className="footer_link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#skills" className="footer_link">
+              Skills
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer_link">
+              Portfolio
+            </a>
+          </li>
+
+          <li>
+            <a href="#contact" className="footer_link">
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer_social">
+          <a href="" className="footer_social-link" target="_blank">
+            <i className="uil uil-facebook"></i>
+          </a>
+          <a href="" className="footer_social-link" target="_blank">
+            <i className="uil uil-linkedin-alt"></i>
+          </a>
+          <a href="" className="footer_social-link" target="_blank">
+            <i className="uil uil-github-alt"></i>
+          </a>
+        </div>
+
+        <span className="footer_copy">&#169;Shehan Lakshitha. All rights reserved</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
